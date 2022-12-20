db8fc74f4886d836346b38eec9950ee4a6a551ea
Андрей <parabellum.koval@gmail.com>
2022-12-20 18:17:17 +0200

    resources change

---
+++ src/app/Http/Resources/ProfileFullResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Backpack\Profile\app\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProfileTinyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+      return [
+        'id' => $this->id,
+        'login' => $this->login,
+        'firstname' => $this->firstname,
+        'lastname' => $this->lastname,
+        'fullname' => $this->fullname,
+        'phone' => $this->phone,
+        'photo' => $this->photo? url($this->photo): null,
+        'email' => $this->email,
+        'referrer_id' => $this->referrer_id,
+        'referral_code' => $this->referral_code,
+        'addresses' => $this->addresses,
+        'extras' => $this->extras,
+        'created_at' => $this->created_at->format('d.m.Y'),
+      ];
+    }
+}
