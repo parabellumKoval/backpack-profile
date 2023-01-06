f2e5dc3d761e8a4f4470bab8ac82abfafb934608
Андрей <parabellum.koval@gmail.com>
2023-01-06 21:34:29 +0200

    fix Auth

--- src/app/Http/Controllers/Auth/RegisterController.php
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
 
 use Backpack\Profile\app\Models\Profile;
 
