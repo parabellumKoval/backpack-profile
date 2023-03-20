f373653c327a86b66bfc7fcd30565c82f224b794
Андрей <parabellum.koval@gmail.com>
2023-03-20 22:02:12 +0200

    add referrals controller

--- src/routes/backpack/routes.php
+++ src/routes/backpack/routes.php
@@ -8,3 +8,11 @@ Route::group([
     Route::crud('profile', 'ProfileCrudController');
 }); 
 
+Route::group([
+  'namespace'  => 'Backpack\Profile\app\Http\Controllers\Admin',
+  'middleware' => ['web', config('backpack.base.middleware_key', 'admin')],
+  'prefix'     => config('backpack.base.route_prefix', 'admin'),
+], function () { 
+  Route::crud('referrals', 'ReferralsCrudController');
+}); 
+
